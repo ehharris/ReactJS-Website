e4a426e0523aaee47d109087fe71278869000a6f
TylerDansby <42559191+TylerDansby@users.noreply.github.com>
2018-09-19 17:26:07 -0600

    Update Distance.java

--- server/src/main/java/com/tripco/t10/planner/Distance.java
+++ server/src/main/java/com/tripco/t10/planner/Distance.java
@@ -2,8 +2,7 @@ package com.tripco.t10.planner;
 
 import java.lang.Math;
 
-
-public class Distance {
+public class Distance extends Vincenty {
 
     private String type;
     private int version;
@@ -11,40 +10,12 @@ public class Distance {
     private Place destination;
     private String units;
     private int distance;
-    
-    public int getDistance(){
-        return this.distance;
-    }
 
-    public void calculateDistance(){
-
-
-        double x1, x2, y;
-        x1 = Math.toRadians(this.origin.latitude);
-        x2 = Math.toRadians(this.destination.latitude);
-        y = Math.abs(Math.toRadians(this.destination.longitude) - Math.toRadians(this.origin.longitude));
-
-        //Vincenty Formula
-        double numerator = Math.sqrt((Math.pow (Math.cos(x2) * Math.sin(y),2)) + Math.pow(( Math.cos(x1) * Math.sin(x2) - Math.sin(x1)* Math.cos(x2) * Math.cos(y) ),2));
-        double denominator = (Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(y));;
-        double scalar = Math.atan2(numerator,denominator);
-
-        double e;
-        
-        //Check units
-        if(this.units.equals("miles")){
-            e = 3959 * scalar;
-        } else if(this.units.equals("kilometers")){
-            e = 6371 * scalar;
-        } else {
-            e = 3440 * scalar;
-        }
-
-        this.distance = (int)Math.round(e);
-
-
-    }
-
-
+    //Used in Calculate.java for the distance TFFI.
+    public int getDistance(){ return this.distance; }
+    public Place getOrigin() { return this.origin; }
+    public Place getDestination() { return this.destination; }
+    public String getUnits() { return this.units; }
+    public void setDistance(int input){ this.distance = input; }
 
 }
