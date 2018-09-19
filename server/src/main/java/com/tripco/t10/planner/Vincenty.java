05a70a2e1804a7d10145f5727e7b9cc8448b4803
TylerDansby <42559191+TylerDansby@users.noreply.github.com>
2018-09-18 20:55:41 -0600

    Added Vincenty parent class

---
+++ server/src/main/java/com/tripco/t10/planner/Vincenty.java
@@ -0,0 +1,14 @@
+package com.tripco.t10.planner;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.tripco.t10.planner.Distance;
+import com.tripco.t10.server.HTTP;
+import spark.Request;
+
+public class Vincenty{
+
+
+
+}
