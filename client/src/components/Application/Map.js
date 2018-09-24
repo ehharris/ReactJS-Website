e3a5001f58184cb699f784cf2db1ab246b77fea8
ehharris <ehharris@rams.colostate.edu>
2018-09-24 16:24:57 -0600

    Add map to client and update default port to 31410

---
+++ client/src/components/Application/Map.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {Card, CardBody} from 'reactstrap'
+
+
+export default class Map extends Component {
+    render() {
+        return (
+            <div>
+                <Card>
+                    <CardBody>
+                        <p className="lead">"A Map of the Whole Wide Colorado"</p>
+                        <img src={require('./Resources/CObackground.svg')}/>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+}
