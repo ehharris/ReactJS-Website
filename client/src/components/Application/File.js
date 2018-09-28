9cf42054f721ccc78f2a01cf80d3dc09be39346e
TylerDansby <42559191+TylerDansby@users.noreply.github.com>
2018-09-27 23:24:16 -0600

    Update File.js

--- client/src/components/Application/File.js
+++ client/src/components/Application/File.js
@@ -4,18 +4,51 @@ import {
     FormGroup, Label, Input,
     Button, CardBody, ButtonGroup, Card,
 } from 'reactstrap';2
+import App from './Application'
 
-/* Options allows the user to change the parameters for planning
- * and rendering the trip map and itinerary.
- * The options reside in the parent object so they may be shared with the Trip object.
- * Allows the user to set the options used by the application via a set of buttons.
- */
 class File extends Component{
     constructor(props) {
         super(props);
+        this.changefileName=this.changefileName.bind(this);
+        this.state = {
+            config: null,
+            trip: {
+                type: "trip",
+                title: "",
+                options : {
+                    unit: "miles"
+                },
+                places: [],
+                distances: [],
+                map: '<svg width="1920" height="20" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><g></g></svg>'
+            }
+        };
+
     }
 
+    //Method taken from piazza - Caleb Carlson
+    changefileName(event){
+        let file = event.target.files[0]; // first file in the FileList
+        if (file) {
+            let reader = new FileReader();
+            reader.onload = function(event) {
+                
+                let newfile = JSON.parse(event.target.result);
+                
+                return function(e){
+                    this.setState({trip:newfile});
+                }
+
+            };
+            reader.readAsText(file);
+        }
+
+
+}
+
+
     render() {
+
 
 
         return(
@@ -24,17 +57,14 @@ class File extends Component{
                     <Form>
                         <FormGroup>
                             <Label for="exampleFile">File</Label>
-                            <Input type="file" name="file" id="exampleFile" />
+                            <Input type="file" name="file" id="exampleFile" onChange={this.changefileName}/>
+                            {/*<Button*/}
+                                {/*onClick={(event) => this.props.uploadFile(event)}*/}
+                            {/*>*/}
+                                {/*Submit*/}
+                            {/*</Button>*/}
                         </FormGroup>
-                        <ButtonGroup>
-                            <Button
-                                key={'distance_button_'}
-                                className='btn-outline-dark unit-button'
-                                onClick={(event) => this.props.updateFile(file)}
-                            >
-                                Submit
-                            </Button>
-                        </ButtonGroup>
+
                     </Form>
                 </CardBody>
             </Card>
