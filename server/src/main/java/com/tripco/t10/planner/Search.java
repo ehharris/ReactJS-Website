ad86e216fe63ece95919dcea6cb11d2eeb3626d1
nkaliher <33334014+nkaliher@users.noreply.github.com>
2018-10-03 17:44:02 -0600

    Added Search.java

---
+++ server/src/main/java/com/tripco/t10/planner/Search.java
@@ -0,0 +1,13 @@
+package com.tripco.t10.planner;
+
+import java.util.ArrayList;
+
+public class Search {
+
+    private String type;
+    private int version;
+    private String match;
+    private int limit;
+    private ArrayList<Place> places;
+
+}
