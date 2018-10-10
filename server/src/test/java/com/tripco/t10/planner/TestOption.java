908b75eccff5f21010ed0767ed412b2718fb3cb4
nkaliher <33334014+nkaliher@users.noreply.github.com>
2018-10-09 18:14:07 -0600

    Added TestOption Class

---
+++ server/src/test/java/com/tripco/t10/planner/TestOption.java
@@ -0,0 +1,12 @@
+package com.tripco.t10.planner;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+public class TestOption {
+
+}
