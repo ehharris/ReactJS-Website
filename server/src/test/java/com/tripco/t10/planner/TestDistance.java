93f3f5839b746803262cec2f04beef25f68e56ef
nkaliher <33334014+nkaliher@users.noreply.github.com>
2018-10-09 21:00:12 -0600

    Added TestDistance Class

---
+++ server/src/test/java/com/tripco/t10/planner/TestDistance.java
@@ -0,0 +1,15 @@
+package com.tripco.t10.planner;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+/*
+  This class contains tests for the Distance class.
+ */
+public class TestDistance {
+
+}
