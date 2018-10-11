845a3027c098f0d0d0b1e62baf8a9da053b9aaae
TylerDansby <42559191+TylerDansby@users.noreply.github.com>
2018-10-10 18:08:20 -0600

    Create Optimization.java

---
+++ server/src/main/java/com/tripco/t10/planner/Optimization.java
@@ -0,0 +1,11 @@
+package com.tripco.t10.planner;
+
+public class Optimization {
+    public String label;
+    public String description;
+
+    public Optimization(String label, String description){
+        this.label = label;
+        this.description = description;
+    }
+}
