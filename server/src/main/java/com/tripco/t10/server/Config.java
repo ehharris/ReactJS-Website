eaa881db84cfa5cc02bf3e73bcc58527c5a5cd51
nkaliher <33334014+nkaliher@users.noreply.github.com>
2018-10-11 19:15:26 -0600

    Update Config.java

--- server/src/main/java/com/tripco/t10/server/Config.java
+++ server/src/main/java/com/tripco/t10/server/Config.java
@@ -11,7 +11,8 @@ public class Config {
   private short version = 3;
   private String type = "config";
 
-  private List<String> units = Arrays.asList("miles", "kilometers", "nautical miles", "user defined");
+  private List<String> units = Arrays.asList("miles", "kilometers", 
+                                             "nautical miles", "user defined");
   private List<Optimization> optimization = Arrays.asList(
       new Optimization("none","The trip is not optimized."),
       new Optimization("short","Nearest neighbor."),
