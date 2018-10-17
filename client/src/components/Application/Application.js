a5db994db198f63c132de577e75e1bac8ab6bf9c
ehharris <ehharris@rams.colostate.edu>
2018-10-17 16:55:10 -0600

    Merge remote-tracking branch 'remotes/origin/master' into AddIT
    # Conflicts:
    #	client/src/components/Application/Application.js

--- client/src/components/Application/Application.js
+++ client/src/components/Application/Application.js
@@ -24,7 +24,7 @@ class Application extends Component {
         options : {
           unit: ""
         },
-        places: [{}],
+        places: [],
         distances: [],
         map: '<svg width="1920" height="20" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><g></g></svg>'
       }
