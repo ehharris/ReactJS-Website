8d86e72b30b0e561e2dd7e649da3c27cad0f503d
nkaliher <nickkaliher@gmail.com>
2018-10-18 16:46:19 -0600

    Added Another Test and Modified Options to fit new Trip

--- server/src/test/java/com/tripco/t10/planner/TestTrip.java
+++ server/src/test/java/com/tripco/t10/planner/TestTrip.java
@@ -30,7 +30,7 @@ public class TestTrip {
     listOfPlaces.add(testBoulder);
     listOfPlaces.add(testFortCollins);
 
-    Option testOption = new Option("miles", 3959, "none");
+    Option testOption = new Option("user defined", 3959, "short", "miles");
 
     ArrayList<Integer> distances = new ArrayList<>();
     distances.add(0);
@@ -40,6 +40,8 @@ public class TestTrip {
     String testMap = "\"<svg width=\\\"1920\\\" height=\\\"960\\\" xmlns=\\\"http://www.w3.org/2000/svg\\\" xmlns:svg=\\\"http://www.w3.org/2000/svg\\\"><!-- Created with SVG-edit - http://svg-edit.googlecode.com/ --> <g> <g id=\\\"svg_4\\\"> <svg id=\\\"svg_1\\\" height=\\\"960\\\" width=\\\"1920\\\" xmlns:svg=\\\"http://www.w3.org/2000/svg\\\" xmlns=\\\"http://www.w3.org/2000/svg\\\"> <g id=\\\"svg_2\\\"> <title>Layer 1</title> <rect fill=\\\"rgb(119, 204, 119)\\\" stroke=\\\"black\\\" x=\\\"0\\\" y=\\\"0\\\" width=\\\"1920\\\" height=\\\"960\\\" id=\\\"svg_3\\\"/> </g> </svg> </g> <g id=\\\"svg_9\\\"> <svg id=\\\"svg_5\\\" height=\\\"480\\\" width=\\\"960\\\" y=\\\"240\\\" x=\\\"480\\\" xmlns:svg=\\\"http://www.w3.org/2000/svg\\\" xmlns=\\\"http://www.w3.org/2000/svg\\\"> <g id=\\\"svg_6\\\"> <title>Layer 2</title> <polygon points=\\\"0,0 960,0 960,480 0,480\\\" stroke-width=\\\"12\\\" stroke=\\\"brown\\\" fill=\\\"none\\\" id=\\\"svg_8\\\"/> <polyline points=\\\"0,0 960,480 480,0 0,480 960,0 480,480 0,0\\\" fill=\\\"none\\\" stroke-width=\\\"4\\\" stroke=\\\"blue\\\" id=\\\"svg_7\\\"/> </g> </svg> </g> </g> </svg>\";\n" + "  }";
 
     trip = new Trip(2, "trip", "Test", listOfPlaces, testOption, distances, testMap);
+
+    trip.plan();
   }
 
   @Test
@@ -49,7 +51,6 @@ public class TestTrip {
 
   @Test
   public void testLegDistances() {
-    trip.plan();
 
     ArrayList<Integer> expectedDistances = new ArrayList<Integer>();
     Collections.addAll(expectedDistances, 24,41,59);
@@ -57,10 +58,26 @@ public class TestTrip {
     assertEquals(expectedDistances, trip.distances);
   }
 
-  @Test 
-  public void testMap() {
-//    trip.plan();
-//    assertEquals(trip.map,"<svg width=\"1920\" height=\"960\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><!-- Created with SVG-edit - http://svg-edit.googlecode.com/ --> <g> <g id=\"svg_4\"> <svg id=\"svg_1\" height=\"960\" width=\"1920\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_2\"> <title>Layer 1</title> <rect fill=\"rgb(119, 204, 119)\" stroke=\"black\" x=\"0\" y=\"0\" width=\"1920\" height=\"960\" id=\"svg_3\"/> </g> </svg> </g> <g id=\"svg_9\"> <svg id=\"svg_5\" height=\"480\" width=\"960\" y=\"240\" x=\"480\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_6\"> <title>Layer 2</title> <polygon points=\"0,0 960,0 960,480 0,480\" stroke-width=\"12\" stroke=\"brown\" fill=\"none\" id=\"svg_8\"/> <polyline points=\"0,0 960,480 480,0 0,480 960,0 480,480 0,0\" fill=\"none\" stroke-width=\"4\" stroke=\"blue\" id=\"svg_7\"/> </g> </svg> </g> </g> </svg>");
+  @Test
+  public void testShortOptimization(){
+    ArrayList<Place> testShort = new ArrayList<>();
+
+    Place testDenver = new Place("dnvr", "Denver", 39.7392, -104.9903);
+    Place testBoulder = new Place("bldr", "Boulder", 40.01499, -105.27055);
+    Place testFortCollins = new Place("foco", "Fort Collins", 40.585258, -105.084419);
+
+    testShort.add(testFortCollins);
+    testShort.add(testBoulder);
+    testShort.add(testDenver);
+
+    if(trip.places.size() == testShort.size() && trip.places != null && testShort != null){
+      for(Place place: trip.places){
+        if(!testShort.contains(place)){
+          assertFalse(false);
+        }
+      }
+    }
+
 
   }
 }
