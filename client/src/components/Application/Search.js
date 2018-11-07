b09801929920f665b6caadd5883818e6f177bbf0
[Antonio Maldonado] <[valid-email]>
2018-11-07 01:15:11 -0700

    Search Updates Itinerary

--- client/src/components/Application/Search.js
+++ client/src/components/Application/Search.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
-import { Card, CardHeader, CardBody } from 'reactstrap'
-
+import { Card, CardHeader, CardBody, Button } from 'reactstrap';
 import { request } from '../../api/api';
 
 /* Allows the user to search from database.
@@ -12,10 +11,18 @@ class Search extends Component{
         this.updateSearch = this.updateSearch.bind(this);
         this.updateLimit = this.updateLimit.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.updatePlace = this.updatePlace.bind(this);
+        // this.sendToParent = this.sendToParent.bind(this);
         this.state = {
             search: '',
             limit: '',
-            results: ''
+            results: [],
+            place: {
+                id: '',
+                name: '',
+                latitude: 0,
+                longitude: 0,
+            }
         };
     }
 
@@ -30,8 +37,7 @@ class Search extends Component{
     handleSubmit(event) {
         event.preventDefault();
 
-        request({ "version": 3, "type": "search", "match": this.state.search, "limit": this.state.limit }, "search", this.props.port, this.props.server).then((resData) => this.setState({results: JSON.stringify(resData)}));
-
+        request({ "version": 3, "type": "search", "match": this.state.search, "limit": this.state.limit }, "search", this.props.port, this.props.server).then((resData) => this.setState({results: resData}));
 
         // fetch('http://' + this.props.server + ":" + this.props.port + "/search", {
         //     method: 'POST',
@@ -50,21 +56,143 @@ class Search extends Component{
         //     .then(resData => this.setState({results: JSON.stringify(resData)}));
     }
 
+    buildIdResults() {
+        let idData = [];
+        if(this.state.results.places) {
+            this.state.results.places.map((key, index, array) => {
+                if(index > 0) {
+                    idData[index] = this.state.results.places[index].id;
+                }
+            });
+        }
+        return idData;
+    }
+
+    buildNameResults() {
+        let nameData = [];
+        if(this.state.results.places) {
+            this.state.results.places.map((key, index, array) => {
+                if (index > 0) {
+                    nameData[index] = this.state.results.places[index].name;
+                }
+            });
+        }
+        return nameData;
+    }
+
+    buildLatitudeResults() {
+        let latitudeData = [];
+        if(this.state.results.places) {
+            this.state.results.places.map((key, index, array) => {
+                if(index > 0) {
+                    latitudeData[index] = this.state.results.places[index].latitude;
+                }
+            });
+        }
+        return latitudeData;
+    }
+
+    buildLongitudeResults() {
+        let longitudeData = [];
+        if(this.state.results.places) {
+            this.state.results.places.map((key, index, array) => {
+                if(index > 0) {
+                    longitudeData[index] = this.state.results.places[index].longitude;
+                }
+            });
+        }
+        return longitudeData;
+    }
+
+    //add method that sends to props
+
+    updatePlace(event) {
+        let index = event.target.value;
+        let place = this.state.results.places[index];
+        let trip = this.props.trip;
+
+        if(trip.places){
+            trip.places = [place];
+            console.log("If");
+            console.log(trip.places);
+        }
+        else {
+            trip.places.push(place);
+            console.log("else");
+            console.log(trip.places);
+        }
+
+        this.props.updateBasedOnResponse(trip);
+
+        // this.setState({place: {id: this.state.results.places[index].id,
+        //                         name: this.state.results.places[index].name,
+        //                         latitude: this.state.results.places[index].latitude,
+        //                         longitude: this.state.results.places[index].longitude}});
+        // this.sendToParent();
+    }
+
     render() {
+        let idData = this.buildIdResults();
+        let nameData = this.buildNameResults();
+        let latitudeData = this.buildLatitudeResults();
+        let longitudeData = this.buildLongitudeResults();
+
+        let resultsNotEmpty = false;
+        if(this.state.results.places) {
+            resultsNotEmpty = true;
+        }
 
         return(
             <Card>
                 <CardBody>
                     <form onSubmit={this.handleSubmit}>
-                          <div> Search trip destinations: </div>
-                          <input name="Search Entry" type="text" value={this.state.search} onChange={this.updateSearch} />
-                          <div> Enter number to limit search results or leave empty to display all results: </div>
-                          <input name="Limit Field" type="text" value={this.state.limit} onChange={this.updateLimit} />
-                          <button value="submit" className="btn-outline-dark unit-button" onClick={this.handleSubmit}>Submit</button>
+                        <div> Search trip destinations: </div>
+                        <input name="Search Entry" type="text" value={this.state.search} onChange={this.updateSearch} />
+                        <div> Enter number to limit search results or leave empty to display all results: </div>
+                        <input name="Limit Field" type="text" value={this.state.limit} onChange={this.updateLimit} />
+                        <button value="submit" className="btn-outline-dark unit-button" onClick={this.handleSubmit}>Submit</button>
                     </form>
 
                     <div>Data Received: </div><br />
-                    {this.state.results}
+                    <div>
+                        {resultsNotEmpty ? (
+                            <table>
+                                <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th>Latitude</th>
+                                    <th>Longitude</th>
+                                    <th>Add</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.state.results.places.map((key,index,array)=> (
+                                    <tr>
+                                        <td key={idData[index]}>
+                                            {idData[index]}
+                                        </td>
+                                        <td key={nameData[index]}>
+                                            {nameData[index]}
+                                        </td>
+                                        <td key={latitudeData[index]}>
+                                            {latitudeData[index]}
+                                        </td>
+                                        <td key={longitudeData[index]}>
+                                            {longitudeData[index]}
+                                        </td>
+                                        <td>
+                                            <Button value={index} type="button" className="btn-outline-dark unit-button" onClick={this.updatePlace}>+</Button>
+                                        </td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        ) : (
+                            <div></div>
+                        )}
+
+                    </div>
                 </CardBody>
             </Card>
         )
