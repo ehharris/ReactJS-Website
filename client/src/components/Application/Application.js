c609a2c873f4882a64b5746925f4cef269c4014b
TylerDansby <tsdansby@rams.colostate.edu>
2018-11-15 18:01:23 -0700

    Added setCookie and checkCookie functions.

--- client/src/components/Application/Application.js
+++ client/src/components/Application/Application.js
@@ -42,6 +42,7 @@ class Application extends Component {
         this.updateServer = this.updateServer.bind(this);
         this.updatePlaces = this.updatePlaces.bind(this);
         this.toggle = this.toggle.bind(this);
+        this.checkCookie()
     }
 
     componentWillMount() {
@@ -110,6 +111,25 @@ class Application extends Component {
         }
       }
       return "";
+    }
+
+    setCookie(cname, cvalue, exdays) {
+      let d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      let expires = "expires="+d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    checkCookie() {
+      let user = this.getCookie("username");
+      if (user !== "") {
+        alert("Welcome again " + user);
+      } else {
+        user = prompt("Please enter your name:", "");
+        if (user !== "" && user != null) {
+          this.setCookie("username", user, 365);
+        }
+      }
     }
 
     renderNav(){
